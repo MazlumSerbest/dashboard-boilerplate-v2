b0395790867fbe17f8d699089e77868e81670ca7
MazlumSerbest <mazlumserbest93@gmail.com>
2024-04-04 11:32:22 +0300

    Sample pages added

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,216 @@
+import Link from "next/link"
+import {
+  Bell,
+  CircleUser,
+  Cog,
+  Home,
+  Menu,
+  Package2,
+  Search,
+  Users2,
+} from "lucide-react"
+
+// import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <div className="hidden border-r bg-muted/40 md:block">
+                <div className="flex h-full max-h-screen flex-col gap-2">
+                    <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+                        <Link
+                            href="/"
+                            className="flex items-center gap-2 font-semibold"
+                        >
+                            <Package2 className="size-6" />
+                            <span className="">Acme Inc</span>
+                        </Link>
+                        <Button
+                            variant="outline"
+                            size="icon"
+                            className="ml-auto size-8"
+                        >
+                            <Bell className="size-4" />
+                            <span className="sr-only">
+                                Toggle notifications
+                            </span>
+                        </Button>
+                    </div>
+                    <div className="flex-1">
+                        <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                            <Link
+                                href="/dashboard"
+                                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+                            >
+                                <Home className="size-4" />
+                                Dashboard
+                            </Link>
+                            <Link
+                                href="/dashboard/users"
+                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                            >
+                                <Users2 className="size-4" />
+                                Users
+                                {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                                    6
+                                </Badge> */}
+                            </Link>
+                            <Link
+                                href="/dashboard/settings"
+                                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+                            >
+                                <Cog className="size-4" />
+                                Settings{" "}
+                            </Link>
+                        </nav>
+                    </div>
+                    {/* <div className="mt-auto p-4">
+                        <Card>
+                            <CardHeader className="p-2 pt-0 md:p-4">
+                                <CardTitle>Upgrade to Pro</CardTitle>
+                                <CardDescription>
+                                    Unlock all features and get unlimited access
+                                    to our support team.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+                                <Button size="sm" className="w-full">
+                                    Upgrade
+                                </Button>
+                            </CardContent>
+                        </Card>
+                    </div> */}
+                </div>
+            </div>
+            <div className="flex flex-col">
+                <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button
+                                variant="outline"
+                                size="icon"
+                                className="shrink-0 md:hidden"
+                            >
+                                <Menu className="size-5" />
+                                <span className="sr-only">
+                                    Toggle navigation menu
+                                </span>
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent side="left" className="flex flex-col">
+                            <nav className="grid gap-2 text-lg font-medium">
+                                <Link
+                                    href="#"
+                                    className="flex items-center gap-2 text-lg font-semibold"
+                                >
+                                    <Package2 className="size-6" />
+                                    <span className="sr-only">Acme Inc</span>
+                                </Link>
+                                <Link
+                                    href="/dashboard"
+                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+                                >
+                                    <Home className="size-5" />
+                                    Dashboard
+                                </Link>
+                                <Link
+                                    href="/dashboard/users"
+                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                                >
+                                    <Users2 className="size-5" />
+                                    Users
+                                    {/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                                        6
+                                    </Badge> */}
+                                </Link>
+                                <Link
+                                    href="/dashboard/settings"
+                                    className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                                >
+                                    <Cog className="size-5" />
+                                    Settings
+                                </Link>
+                            </nav>
+                            {/* <div className="mt-auto">
+                                <Card>
+                                    <CardHeader>
+                                        <CardTitle>Upgrade to Pro</CardTitle>
+                                        <CardDescription>
+                                            Unlock all features and get
+                                            unlimited access to our support
+                                            team.
+                                        </CardDescription>
+                                    </CardHeader>
+                                    <CardContent>
+                                        <Button size="sm" className="w-full">
+                                            Upgrade
+                                        </Button>
+                                    </CardContent>
+                                </Card>
+                            </div> */}
+                        </SheetContent>
+                    </Sheet>
+                    <div className="w-full flex-1">
+                        <form>
+                            <div className="relative">
+                                <Search className="absolute left-2.5 top-2.5 size-4 text-muted-foreground" />
+                                <Input
+                                    type="search"
+                                    placeholder="Search products..."
+                                    className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                                />
+                            </div>
+                        </form>
+                    </div>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button
+                                variant="secondary"
+                                size="icon"
+                                className="rounded-full"
+                            >
+                                <CircleUser className="size-5" />
+                                <span className="sr-only">
+                                    Toggle user menu
+                                </span>
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Settings</DropdownMenuItem>
+                            <DropdownMenuItem>Support</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Logout</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </header>
+                <main className="flex-1">
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/dashboard/users/page.tsx
@@ -0,0 +1,9 @@
+export default function Users() {
+    return (
+        <section className="flex flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+            <div className="flex items-center">
+                <h1 className="text-lg font-semibold md:text-2xl">Users</h1>
+            </div>
+        </section>
+    );
+}
